7cb00a32801740697512d705ffe91e95074df271
Ophélie Rochefeuille <ophelie.rochefeuille@green-conseil.com>
2023-02-27 19:45:38 +0100

    Password open OS

--- src/var.js
+++ src/var.js
@@ -1,9 +1,37 @@
-export let test = "hello";
-export function test2() {
-    return "JE suis Hello"
+/*export let test = "hello";
+//export function test2() {
+  //  return "JE suis Hello"
 }
-export let horloge = "/components/horloge/horloge.html";
-export let calculatrice = "/components/calculatrice/calculatrice.html";
-export let ticTacToe = "/components/tictactoe/tictactoe.html";
-export let parametre = "/components/parametres/parametre.html";
-// export {test, test2};
+//export let horloge = "/components/horloge/horloge.html";
+//export let calculatrice = "/components/calculatrice/calculatrice.html";
+//export let ticTacToe = "/components/tictactoe/tictactoe.html";
+//export let parametre = "/components/parametres/parametre.html";
+// export {test, test2};*/
+
+let pwd = "javascript"
+let pwdEnter = document.querySelector('.password').value
+function openModal(id) {
+    document.getElementById(id).classList.add('open');
+    document.body.classList.add('jw-modal-open');
+}
+
+openModal("1")
+// close currently open modal
+function closeModal() {
+    if(document.querySelector('.password').value == pwd){
+        document.querySelector('.jw-modal.open').classList.remove('open');
+        document.body.classList.remove('jw-modal-open');
+    }
+
+}
+
+window.addEventListener('load', function() {
+
+    document.addEventListener('click', event => {
+        if (event.target.classList.contains('jw-modal')) {
+            console.log(document.querySelector('.password').value)
+            closeModal();
+        }
+    });
+});
+

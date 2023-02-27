5fa75b72830af16d6e5ef1b2e1205bcc3d485374
Elodie Jolo <elodie.jolo@gmail.com>
2023-02-27 15:27:59 +0100

    merge

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,3 +7,12 @@ function openHorloge()
     window.open('/components/horloge/horloge.html', 'test', 'width='+width+', height='+height+', top='+top+', left='+left+'');
 }
 
+function openTicTacToe()
+{
+    width = screen.width/2;
+    height = screen.height/2 ;
+    top = (screen.height/2)-(width/2);
+    left = (screen.width/2)-(height/2);
+    window.open('/components/game/tictactoe.html', 'test', 'width='+width+', height='+height+', top='+top+', left='+left+'');
+}
+

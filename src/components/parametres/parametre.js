305d404d7ba5a5e03428dd05aac4b8aff6de3bef
DrAtsiSama <dareka@outlook.fr>
2023-02-27 20:13:31 +0100

    DarkMode

--- src/components/parametres/parametre.js
+++ src/components/parametres/parametre.js
@@ -1,54 +1,47 @@
 // import {switchDarkMode,hello} from("../darkMode/darkMode.js");
-
+let parametrage = {};
+init();
 // switchDarkMode();
-
 // Enregistre un objet dans le stockage local
-let parametrage = { 
-    latence_Reseau: {
-        affichage_latence : true,
-        domainePing : "http://127.0.0.1:8080/",
-        delaisRafraichissement : "60"
-    },
-    formatHoraire: {
-        affichage_heure : true,
-        affichage_minute : true,
-        affichage_seconde : true
-    },
-    formatDate: {
-        affichage_date : true,
-        affichage_annee : true,
-        affichage_mois : true,
-        affichage_jour : true
-    },
-    vibration: {
-        retour_haptique : true,
-        etat_vibration : "silencieux"
-    },
-    etatBatterie : true,
-    theme: "light",
-    verrouillage: "veille"
-};
-localStorage.setItem('parametrages', JSON.stringify(parametrage));
-
-// Récupère l'objet du stockage local
-let parametragetest = getLocalStorage();
-for(let i =0; i < parametragetest.length(); i++){
-  console.log(parametragetest[i]);
+function init() {
+    if(isLocalStorage() == false) {
+        this.parametrage = { 
+            latence_Reseau: {
+                affichage_latence : true,
+                domainePing : "",
+                delaisRafraichissement : ""
+            },
+            formatHoraire: {
+                affichage_heure : true,
+                affichage_minute : true,
+                affichage_seconde : true
+            },
+            formatDate: {
+                affichage_date : true,
+                affichage_annee : true,
+                affichage_mois : true,
+                affichage_jour : true
+            },
+            vibration: {
+                retour_haptique : true,
+                etat_vibration : ""
+            },
+            etatBatterie : true,
+            theme: "",
+            verrouillage: ""
+        };
+        localStorage.setItem('parametrages', JSON.stringify(this.parametrage));
+    } else {
+        this.parametrage = getLocalStorage();
+    }
 }
-// Modifie un champ de l'objet
-parametragetest.verrouillage = "eteint";
-
+// Récupère l'objet du stockage local
+// let parametragetest = getLocalStorage();
 // Enregistre l'objet modifié dans le stockage local
-localStorage.setItem('parametrages', JSON.stringify(parametragetest));
-
-// Récupère l'objet modifié du stockage local
-// parametrageJSON = localStorage.getItem('parametrages');
-// parametrageObjet = JSON.parse(parametrageJSON);
+// localStorage.setItem('parametrages', JSON.stringify(setLocalStorage(parametragetest, "eteint")));// JSON.stringify(parametragetest));
 
 
-
-
-
+console.log("JSON PARTY : ", JSON.stringify(this.parametrage));
 //                                                      Latence Réseau
 
 
@@ -56,31 +49,27 @@ localStorage.setItem('parametrages', JSON.stringify(parametragetest));
 
 
 
-let test = JSON.parse(localStorage.getItem("parametrages"));
-console.log(test);
-let test2 = JSON.parse(test)
-console.log(test2.theme);
-
 //                                                      Switch Thème
 // Récupère le choix de thème enregistré
-let theme = test.theme;//localStorage.getItem('theme');
+let parametreTheme = getLocalStorage();
 // Applique le thème
 let themeSelector = document.getElementById('theme-selector');
-if (theme) {
-  themeSelector.value = theme;
-  applyTheme(theme);
+if (!!parametreTheme.theme) {
+  themeSelector.value = parametreTheme.theme;
+  applyTheme(parametreTheme.theme);
 } else {
-  parametrageJson.theme = "light"
-localStorage.setItem('parametrages', JSON.stringify(parametrageJSON));
-  localStorage.setItem('theme', 'light'); // Applique le thème par défaut si aucun choix n'a été enregistré
+  parametreTheme.theme = "light"
+localStorage.setItem('parametrages', JSON.stringify(setLSTheme(this.parametrage, "light"))); // Applique le thème par défaut si aucun choix n'a été enregistré
   applyTheme('light');
 }
 // Enregistre le choix de thème lorsqu'il est modifié
 themeSelector.addEventListener('change', function() {
   let selectedTheme = themeSelector.value;
-  localStorage.setItem('theme', selectedTheme);
+  let parametrageTheme = getLocalStorage();
+  localStorage.setItem('parametrages', JSON.stringify(setLSTheme(parametrageTheme, selectedTheme)));
   applyTheme(selectedTheme);
 });
+// localStorage.setItem('parametrages', JSON.stringify(setLSTheme(parametragetest, "light")));// JSON.stringify(parametragetest));
 // Applique le thème sélectionné
 function applyTheme(theme) {
   if (theme === 'dark') {
@@ -97,15 +86,26 @@ function applyTheme(theme) {
 //                                                      Switch Thème (FIN)
 
 
-function setLocalStorage(){
-  let getParametrage = localStorage.getItem('parametrages');
-  let parametrageObject = JSON.parse(parametrageJSON);
+function setLSVerrouillage(parametre, value){
+  parametre.verrouillage = value;
+  return parametre;
 }
+
+function setLSTheme(parametre, value){
+    parametre.theme = value;
+    return parametre;
+  }
 
 function getLocalStorage(){
   // Récupère l'objet du stockage local
 let parametrageJSON = localStorage.getItem('parametrages');
 let parametrageObjet = JSON.parse(parametrageJSON);
-console.log(parametrageObjet.verrouillage); // Affiche {nom: "Dupont", prenom: "Jean", age: 30}
 return parametrageObjet;
+}
+function isLocalStorage(){
+    return !!localStorage.getItem('parametrages');
+}
+
+function resetPSLocalStorage() {
+    localStorage.removeItem("parametrages")
 }

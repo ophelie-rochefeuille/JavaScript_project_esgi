93282755c6167d43ad9479b483f8f4dc518f050f
DrAtsiSama <dareka@outlook.fr>
2023-02-28 09:54:29 +0100

    calculatrice

--- src/components/calculatrice/calculatrice.js
+++ src/components/calculatrice/calculatrice.js
@@ -10,10 +10,18 @@ function clearScreen() {
     function calculate() {
     var p = document.getElementById("result").value;
     var q = eval(p);
+    saveCalcul(p,q);
     document.getElementById("result").value = q;
     }
 
     //resultat intermediaire
+    function saveCalcul(operation, resultat) {
+    document.getElementById("historique").value += " " + operation + " = " + resultat;
+
+    }
+    function clearHistorique(){
+        document.getElementById("historique").value = ""
+    }
     //supprimer resultat intermediaire
     //positif/negatif
     // retour haptique

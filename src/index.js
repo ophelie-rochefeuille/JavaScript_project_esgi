143aebdc284f228e2e0bf0d99b9f79c4c6cb6506
DrAtsiSama <dareka@outlook.fr>
2023-02-28 01:13:03 +0100

    API Wab Animation

--- src/index.js
+++ src/index.js
@@ -24,3 +24,12 @@ function openCalculatrice()
     left = (screen.height/4)-(height/4);
     window.open('./components/calculatrice/calculatrice.html', 'test', 'width='+width+', height='+height+', top='+top+', left='+left+'');
 }
+
+function openWebAnimation()
+{
+    width = screen.width/2; 
+    height = screen.height/2 ;
+    top = (screen.width/2)-(width/2);
+    left = (screen.height/4)-(height/4);
+    window.open('./components/alice/alice.html', 'test', 'width='+width+', height='+height+', top='+top+', left='+left+'');
+}
